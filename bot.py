3bba6144f7099391ce73940d138d54992fb5486d
GamingBeast1 <116881154+GamingBeast1@users.noreply.github.com>
2023-12-30 11:56:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -338,7 +338,7 @@ async def button(bot: Client, cmd: CallbackQuery):
                                              url="https://t.me/EK4MPREETSINGH")
                     ],
                     [
-                        InlineKeyboardButton("◁ʙᴀᴄᴋ", callback_data="gotohome")
+                        InlineKeyboardButton("◁ʙᴀᴄᴋ", callback_data="aboutbot")
                     ]
                 ]
             )
@@ -441,6 +441,19 @@ async def button(bot: Client, cmd: CallbackQuery):
                 ]
             )
         )
+            elif "discliamerdata" in cb_data:
+        await cmd.message.edit(
+            Config.DISCLAIMER_TXT,
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("◁ʙᴀᴄᴋ", callback_data="gotohome")
+                    ]
+                ]
+            )
+        )
+
 
     elif cb_data.startswith("ban_user_"):
         user_id = cb_data.split("_", 2)[-1]
